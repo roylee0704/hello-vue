c041871d837df77416350e0d17d06b3ea16420b9
roylee0704 <roylee0704@gmail.com>
2021-09-09 15:59:54 +0800

    initial commit

---
+++ modal-project-from-vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Modal />
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "My First Vue App :)",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modal-project-from-vue-cli/src/components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>Modal Content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+};
+</script>
+
+<style scoped>
+.backdrop {
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.modal {
+  margin: 100px auto;
+  padding: 20px;
+  width: 400px;
+  border-radius: 10px;
+  background: #fff;
+}
+</style>
